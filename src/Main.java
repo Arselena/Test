0c9c5150086daf399a0ccc5d49a9163e3c57513b
Arselena <73169316+Arselena@users.noreply.github.com>
2022-02-24 21:43:04 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String text1 = "Наша Таня громко плачет:";
+        String text2 = "Уронила в речку мячик";
+        String text3 = "- Тише, Ванечка, не плачь:";
+
+        System.out.print(text1 + '\n' + text2 + '\n' + text3);
+
+    }
+}
